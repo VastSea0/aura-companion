e9f1db8b76c11bf40952d5f43ffd4dc58a03f2e5
VastSea0 <vastseaoffical0@outlook.com>
2024-11-13 00:25:25 +0300

    feat: add test button to explore tab for task creation and update title

--- app/(tabs)/explore.jsx
+++ app/(tabs)/explore.jsx
@@ -3,17 +3,45 @@ import { StyleSheet } from 'react-native';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { TText } from '@/components/TText';
 import { TView } from '@/components/TView';
+import { TButton } from '@/components/TButton';
 
 export default function TabTwoScreen() {
+  
+  const handleTest = () => {
+    console.log('Test button pressed');
+  
+    const url = 'http://192.168.1.161:3000/api/collection/tasks';
+  
+    fetch(url, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ name: 'TaskN', id: 25, completed: false, aura: 50 }),
+      })
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(error => console.error('Error:', error));
+    }
+
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
       headerImage={<Ionicons size={310} name="code-slash" style={styles.headerImage} />}>
       <TView style={styles.titleContainer}>
-        <TText type="title">Explore</TText>
+        <TText type="title">Tests</TText>
       </TView>
-      <TText>This app includes example code to help you get started.</TText>
-      
+      <TText>Egehan'ın proje komponentlerini test ederkene uygulamayı patlatdığı sayfa (ops elim kaydı)</TText>
+      <TButton
+        style={{
+          padding: 10,
+          borderRadius: 8,
+          borderColor: '#ffffff',
+          borderWidth: 1,
+        }}
+        onPress={() => handleTest()}
+        type="pressable"
+        >
+        <TText>ADD TASK TEST BUTTON</TText>
+      </TButton>
     </ParallaxScrollView>
   );
 }
